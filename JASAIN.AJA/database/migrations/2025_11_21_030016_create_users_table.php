bd41573e2151ac01d126646bf565214a9d259dac
HafidzSuhaiby <mohhafidzsuhaiby@gmail.com>
2025-11-22 12:16:55 +0700

    Update 2025_11_21_030016_create_users_table.php
    update tabel user

--- JASAIN.AJA/database/migrations/2025_11_21_030016_create_users_table.php
+++ JASAIN.AJA/database/migrations/2025_11_21_030016_create_users_table.php
@@ -6,30 +6,27 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up()
+    public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('nomor')->nullable();
-            $table->string('alamat')->nullable();
-            $table->string('status')->default('aktif');
 
-            $table->foreignId('role_id')->constrained('roles')->onDelete('cascade');
+            $table->string('name');
+            $table->string('username')->nullable();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+
+            // role_id jika kamu pakai roles table
+            if (Schema::hasTable('roles')) {
+                $table->foreignId('role_id')->nullable()->constrained()->onDelete('set null');
+            }
 
             $table->rememberToken();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
